2225af151d0e4eaebf466db718822b192e24f0ab
yasaminashoori <yasaminashoori18@gmail.com>
2023-06-28 23:06:52 +0430

    Add bookstore info

---
+++ web-api/Repository/BookRepository.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace web_api.Repository
+{
+    public class BookRepository : IBookRepository
+    {
+        
+    }
+}
---
+++ web-api/Repository/IBookRepository.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace web_api.Repository
+{
+    public interface IBookRepository
+    {
+        
+    }
+}
